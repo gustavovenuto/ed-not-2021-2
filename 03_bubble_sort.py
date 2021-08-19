2115b8555fb0a16a63e7cbce05958ac434eec9ef
gustavovenuto <gugu.venuto@gmail.com>
2021-08-19 23:27:20 +0000

    (19/08) Término do bubble sort e início do selection sort

--- 03_bubble_sort.py
+++ 03_bubble_sort.py
@@ -5,25 +5,73 @@
 # menor que o primeiro.Efetua tantas passadas quanto necessárias
 # até que na ultima passada, nenhuma troca seja efetuada
 
+comps = 0
+passadas = 0
+trocas = 0
+
 
 def bubble_sort(lista):
     """
         Função que implementa o algoritimo de ordenação Bubble Sort
     """
 
-    while True: # Loop eterno
+    global comps, passadas, trocas
+
+    comps = 0
+    passadas = 0
+    trocas = 0
+
+    while True:  # Loop eterno
+        passadas += 1
+
         trocou = False
-        #Loop na lista até o penultimo elemento. Porque ele já compara com o ultimo
-        #ex. em uma lista de 10 elementos, len(lista) == 10
-        # A ultima posição estára em len(lista) -1, porcausa que começa a contar apartir de 0, ou seja len(lista) - 1 == 9
-        #A penultima posição será len(lista) -2, ou seja 8
-        
-        for i in range(len(lista)-2): #inicia nova passada
-            if lista[i + 1] < lista[i]: #necessario trocar
+        # Loop na lista até o penultimo elemento. Porque ele já compara com o ultimo
+        # ex. em uma lista de 10 elementos, len(lista) == 10
+        # A ultima posição estára em len(lista) -1, porcausa que começa a contar apartir de 0, ou seja len(lista) - 1 == 9 -> range(len(lista))
+        # A penultima posição será len(lista) -2, ou seja, 8 -> range(len(lista))
+
+        for i in range(len(lista)-1):  # inicia nova passada
+            comps += 1
+            if lista[i + 1] < lista[i]:  # necessario trocar
                 lista[i + 1], lista[i] = lista[i], lista[i + 1]
+                trocas += 1
                 trocou = True
 
         # Se ouve troca, a lista está ordenada e podemos interromper
         # O loop while
         if not trocou:
-            break # Interrompe o while
+            break  # Interrompe o while
+
+# nums = [ 88, 44, 33, 0, 99, 55, 77, 22, 11, 66]
+
+
+# pior caso - nums = [99, 88, 77, 66, 55, 44, 33, 22, 11, 0]
+
+# melhor caso
+nums = [0, 11, 22, 33, 44, 55, 66, 77, 88, 99]
+
+bubble_sort(nums)
+
+print(nums)
+print(f"Comparações: {comps}")
+print(f"Passadas: {passadas}")
+print(f"Trocas: {trocas}")
+
+
+
+#-----------------------------------------------------#
+
+
+from time import time
+from data.nomes_desord import nomes
+
+
+nomes_parcial = nomes[:30000] # usa apenas os primeiros 20 mil nomes
+
+ini = time()
+bubble_sort(nomes_parcial)
+fim = time()
+
+print(nomes_parcial)
+print(f"Tempo: { fim - ini}")
+print(f"Passadas: {passadas}, comparações: {comps}, trocas: {trocas}")
---
+++ 04_selection_sort.py
@@ -0,0 +1,8 @@
+# ALGORITIMO DE ORDENAÇÃO SELECTION SORT
+
+# Isola (seleciona) o primeiro elemento da lista e, em seguida, encontra
+# encontra o menor valor no restante da lista. Se o valor encontrado for menor que o valor selecionado, efetua a troca.
+# Em seguida, isola o segundo número da lista, buscando pelo menor
+# valor da posições subsequentes, faz a troca entre os dois valores,
+# se necessario. continua o processo, até isolar o penúltimo elemento
+# da lista.
