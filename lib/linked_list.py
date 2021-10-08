88e091e8f4913067917f6bfb725bdd1db2a7483d
gustavovenuto <gugu.venuto@gmail.com>
2021-10-08 23:26:18 +0000

    Lista encadeada

--- lib/linked_list.py
+++ lib/linked_list.py
@@ -1,80 +1,149 @@
 """
-    Classe que representa cada unidade elemento da lista encadeada
-    é dividida em duas partes:
-    1) Onde fica armazenada a informação relevante para o usuário (__data)
-    2) O ponteiro para o proximo nodo da sequencia (__next)
+    Classe que representa cada unidade (elemento) da lista encadeada
+    É dividida em duas partes:
+    1) Onde fica armezenada a informação relevante para o usuário (data)
+    2) O ponteiro para o próximo nodo da sequência (next)
 """
-
 class Node:
-    def __init__(self, val):
-        self.__data = val #Armazena informação do usuário
-        self.next = None # Ponteiro para o próximo nodo (None = nenhum)
 
-"""  
-    ESTRUTURA DE DADOS LISTA ENCADEADA    
-    - A lista encadeada é uma estrutura de dados   formada por unidades      
-        de informação chamadas nodos ou nós.    
-    - Cada nodo da lista encadeada tem duas partes: uma, que armazena a      
-        informação e outra que guarda o endereço do próximo nodo da sequência    
-    - A qualquer momento, temos um conhecimento apenas limitado de onde      
-        se encontram todos os nodos da lista. Sabemos apenas onde está o      
-        primeiro e o último nodo da sequência. Os nodos intermediários precisam      
-        ser encontrados partindo-se do primeiro e percorrendo a sequência
+    def __init__(self, val):
+        self.data = val   # Armazena a informação do usuário
+        self.next = None  # Ponteiro para o próximo nodo (None = nenhum)
+
 """
-    
-class LinkedList: 
+    ESTRUTURA DE DADOS LISTA ENCADEADA
+    - A lista encadeada é uma estrutura de dados formada por unidades
+      de informação chamadas nodos ou nós.
+    - Cada nodo da lista encadeada tem duas partes: uma, que armazena a
+      informação e outra que guarda o endereço do próximo nodo da sequência
+    - A qualquer momento, temos um conhecimento apenas limitado de onde
+      se encontram todos os nodos da lista. Sabemos apenas onde está o
+      primeiro e o último nodo da sequência. Os nodos intermediários precisam
+      ser encontrados partindo-se do primeiro e percorrendo a sequência
+"""
+class LinkedList:
+
+    """
+        Construtor da classe
+    """
     def __init__(self):
-        self.__head = None # Ponteiro para o primeiro nodo da lista
-        self.__tail = None # Ponteiro para o ultimo nodo da lista
-        self.__count = 0   # Contador de nodos
+        self.__head = None      # (cabeça) Ponteiro para o primeiro nodo da lista
+        self.__tail = None      # (cauda) Ponteiro para o último nodo da lista
+        self.__count = 0        # Contador de nodos
 
     """
         Método que informa se a lista está ou não vazia
     """
-
     def is_empty(self):
         return self.__count == 0
 
     def insert(self, pos, val):
+
         inserted = Node(val)
 
-        # 1 lista vazia
-        # O nodo criada será, ao mesmo tempo, o primeiro e o ultimo
-
-        if self.is_empty(): 
-            self.head = inserted
+        # 1º caso: a lista está vazia
+        # O nodo criado será, ao mesmo tempo, o primeiro e o último
+        if self.is_empty():
+            self.__head = inserted
             self.__tail = inserted
-        
-        # 2 caso: inserção no incio da lista (posição 0)
+
+        # 2º caso: inserção no início da lista (posição 0)
         elif pos == 0:
             inserted.next = self.__head
             self.__head = inserted
 
+        # 3º caso: inserção no fim da lista
+        # Vamos considerar qualquer posição de inserção >= count
+        # como inserção no final
         elif pos >= self.__count:
             self.__tail.next = inserted
             self.__tail = inserted
+
+        # 4º caso: inserção em posição intermediária
         else:
             before = self.__head
 
-            for i in range(0, pos): 
-                print(f"before.data: {before.data}, i: {i}")
-                before = before.next
-        
+            # Percorre a lista encadeada da segunda posição (pos. 1)
+            # até a posição ANTERIOR àquela de inserção
+            for i in range(1, pos): before = before.next
+
+            # Nodo que ficará DEPOIS do inserido
+            after = before.next
+
+            # O next do nodo inserido passa a ser o after
+            inserted.next = after
+
+            # O next do nodo before passa a ser o inserted
+            before.next = inserted
+
         self.__count += 1
 
-        #print(f"[NODE] data: {inserted.data}, next: {inserted.next}")
+    """
+        Método de atalho para inserção na primeira posição
+    """
+    def insertFront(self, val):
+        self.insert(0, val)
 
+    """
+        Método de atalho para inserção na última posição
+    """
+    def insertBack(self, val):
+        self.insert(self.__count, val)
 
-#####################################################
+    """
+
+        Método para procurar um valor e retornar sua posição
+        retorna -1 caso não encontre.
+    """
+
+    def index(self, val):
+        node = self.__head
+        for pos in range(0, self.__count):
+            if node.data == val : return pos
+            node = node.next
+        return -1
+
+    """
+        Retorna o data do nodo da posição especificada
+    """
+    def peek(self, pos):
+        if self.is_empty() or pos < 0 or pos > self.__count - 1:
+            return None
+        node = self.__head
+        for i in range(0, pos): node = node.next
+        return node.data
+
+    """
+        Método que exibe a lista como uma string (para fins de depuração)
+    """
+    def to_str(self):
+        string = ""
+        node = self.__head
+        for i in range(0, self.__count):
+            if string != "": string += ", "
+            string += f"(pos: {i}, data: {node.data})"
+            node = node.next
+        return "[ " + string + f" ], count: {self.__count}"
+
+##################################################################
 
 lista = LinkedList()
 
 lista.insert(0, "1 kg batata")
 lista.insert(1, "café")
 lista.insert(2, "miojo")
-lista.insert(3, "oleo")
+lista.insert(3, "óleo")
 lista.insert(4, "sabonete")
 lista.insert(5, "shampoo")
 
-
 lista.insert(4, "tomate")
+
+lista.insert(7, "sabão em pó")
+lista.insert(30, "detergente")
+
+#print(lista.to_str())
+
+lista.insertFront("5 kg arroz")
+lista.insertBack("água sanitária")
+
+print(lista.to_str())
